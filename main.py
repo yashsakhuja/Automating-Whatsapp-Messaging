a46ccbd2f07988c44429e1bd88818f18d60ba01c
Yash Sakhuja <yash.sakhuja01@gmail.com>
2024-03-15 21:19:29 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Import Libraries
+import pywhatkit
+import pyautogui
+from tkinter import *
+import datetime
+
+win = Tk()
+screen_width = win.winfo_screenwidth() # Gets the resolution (width) of your monitor
+screen_height= win.winfo_screenheight() # Gets the resolution (height) of your monitor
+
+# Function to schedule message
+def schedule_message(phone_number, message, scheduled_time,screen_width,screen_height):
+            pywhatkit.sendwhatmsg(phone_number, message, scheduled_time.hour, scheduled_time.minute)
+            pyautogui.moveTo(screen_width * 0.694, screen_height* 0.964) # Moves the cursor the the message bar in Whatsapp
+            pyautogui.click() # Clicks the bar
+            pyautogui.press('enter') # Sends the message
+
+sched_time=datetime.datetime(2024, 3, 15, 21, 14,0)
+
+mes="Your Message"
+p_num='+91XXXXXXXXXX'
+
+schedule_message(p_num,mes,sched_time,screen_width,screen_height)
